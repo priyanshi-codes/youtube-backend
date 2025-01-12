e3a4cc099c5072d8f6176ef898fd653f156d7bed
Priyanshi-codes <gupta28priyanshi@gmail.com>
2025-01-13 00:45:31 +0530

    Add user account management features: change password, update details, and avatar/cover image upload

--- src/models/subscription.models.js
+++ src/models/subscription.models.js
@@ -6,7 +6,7 @@ const subscriptionSchema = new Schema({
         ref: "User"
     },
     channel:{
-        type: Schema.Types.ObjectId 
+        type: Schema.Types.ObjectId ,
         ref : "User"
     }
 
